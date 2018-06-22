c02d18611295b6b71c89bad92a46d98c4c8fdc7a
José Iván Chapoñán Casiano <ivan@navike21.com>
2018-06-22 03:05:31 -0500

    Subiendo Archivos a la nuve

--- js/ready.js
+++ js/ready.js
@@ -16,4 +16,32 @@ $(document).ready(function() {
         var image_port = $(this).data("image");
         $(this).css("background-image", "url("+image_port+")");
     });
+
+    function alto_slider(){
+        $(".slider_portafolio").each(function(index, el) {
+            var alto_slider = $(this).height();
+            $(this).find(".image_port").css('height', alto_slider);
+            $(this).find(".slick-list").css('height', alto_slider);
+        });
+    }
+
+    $(window).resize(function(event) {
+        alto_slider();
+    });
+    alto_slider();
+
+    $(".slider_portafolio").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        fade: false,
+        dots: true,
+        pauseOnFocus: false,
+        arrows: false,
+        adaptiveHeight: true,
+        autoplaySpeed: 5000,
+    });
+
+    $(".slider_portafolio").find(".slick-dots").addClass('section_middle_center');
 });
